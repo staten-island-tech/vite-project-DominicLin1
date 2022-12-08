63f9d714e9db021d1b945ddf3a1a77ba0816be68
DominicLin1 <dominiclin777@gmail.com>
2022-12-08 10:13:41 -0500

    commit 3

---
+++ vite-project/js/array.js
@@ -0,0 +1,75 @@
+const League = [
+  {
+    champion: "Yuumi",
+    role: [support],
+    toxic: yes,
+    permaban: yes,
+    price: 6300,
+    img: "https://img.redbull.com/images/c_crop,w_1280,h_640,x_0,y_0,f_auto,q_auto/c_scale,w_1200/redbullcom/2019/05/08/d24d8f2e-8972-466c-9e9f-979f332432fb/yuumi-lol-splash",
+  },
+  {
+    champion: "Irelia",
+    role: [top, mid],
+    toxic: yes,
+    permaban: no,
+    price: 4800,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ04RU_DCUzDkRQ6fsv9PumO1_N4VW40sm_GPIIxUew0g&s",
+  },
+  {
+    champion: "Jinx",
+    role: [adc],
+    toxic: no,
+    permaban: no,
+    price: 4800,
+    img: "https://i.kym-cdn.com/entries/icons/facebook/000/039/062/Jinx_0.jpg",
+  },
+  {
+    champion: "Teemo",
+    role: [top],
+    toxic: yes,
+    permaban: no,
+    price: 1350,
+    img: "https://static.wikia.nocookie.net/leagueoflegends/images/5/5f/Teemo_OriginalCentered.jpg/revision/latest/scale-to-width-down/1280?cb=20180414203624",
+  },
+  {
+    champion: "Akali",
+    role: [mid, top],
+    toxic: yes,
+    permaban: no,
+    price: 3150,
+    img: "https://cdn.oneesports.gg/cdn-data/wp-content/uploads/2019/07/TeamfightTactics_Akali.jpg",
+  },
+  {
+    champion: "Twisted Fate",
+    role: [mid],
+    toxic: no,
+    permaban: no,
+    price: 1350,
+    img: "https://static.wikia.nocookie.net/leagueoflegends/images/4/49/Twisted_Fate_OriginalCentered.jpg/revision/latest/scale-to-width-down/1280?cb=20180414203653",
+  },
+  {
+    champion: "Ryze",
+    role: [mid],
+    toxic: no,
+    permaban: no,
+    price: 450,
+    image:
+      "https://static.wikia.nocookie.net/leagueoflegends/images/e/ed/Ryze_OriginalCentered.jpg/revision/latest/scale-to-width-down/1280?cb=20180414203544",
+  },
+  {
+    champion: "Ezreal",
+    role: [adc],
+    toxic: no,
+    permaban: no,
+    price: 3150,
+    img: "https://static1-br.millenium.gg/entity_articles/2/55/02/@/65409-sa-ezreal-orig-1-orig-1.jpeg",
+  },
+  {
+    champion: "Lee Sin",
+    role:[jungle],
+    toxic: no,
+    permaban: yes,
+    price:
+    img: ""
+  }
+];

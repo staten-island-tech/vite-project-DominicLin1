a0faf654ea445b4a29abf5be75eff3ae776f5a81
DominicLin1 <dominiclin777@gmail.com>
2022-12-19 09:34:03 -0500

    commit

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -2,3 +2,13 @@ import "../styles/style.css" ;
 import { League } from "./array.js";
 /*import { filter } from "./filter";*/
 console.log(League);
+
+function create(){
+const card= {
+    champion:League.champion.value,
+    price:League.price.value,
+    role:League.role.value,
+    permaban:League.permaban.value,
+    toxic:League.toxic.value
+}
+}

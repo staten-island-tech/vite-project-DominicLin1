96a29446de02b8727d35551d2386775811808256
DominicLin1 <dominiclin777@gmail.com>
2022-12-21 10:14:44 -0500

    commit 10

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -2,20 +2,39 @@ import "../styles/style.css" ;
 import { League } from "./array.js";
 /*import { filter } from "./filter";*/
 console.log(League);
+const DOMselectors={
+Menu:document.getElementById("List")}
+
+
 
 function insert(){
-let champion=League.champion.value
-let price=League.price.value
-let role=League.role.value
-let permaban=League.permaban.value
-let toxic=League.toxic.value
-}
+let champion=League.champion
+let price=League.price
+let role=League.role
+let permaban=League.permaban
+let toxic=League.toxic}
+DOMselectors.Menu.addEventListener(
+    "click",
+
 function create(){
-const card= {
-    champion:League.champion.value,
-    price:League.price.value,
-    role:League.role.value,
-    permaban:League.permaban.value,
-    toxic:League.toxic.value
-}
-}
+
+    const card = {
+        champion:League.forEach.champion((League.champion) ),
+        price:League.forEach.price((League.price)  ),
+        role:League.forEach.role((League.role) ),
+        permaban:League.forEach.permaban((League.permaban)  ),
+        toxic:League.forEach.toxic((League.toxic) ),
+        img:League.forEach.img((League.img) )}
+
+DOMselectors.Menu.insertAdjacentHTML(
+    "afterend",
+
+   `<div class="display-card">
+        <img class="display-img" src=${card.img}/>
+        <h2 class="display-champion" id= "display-champion">${card.champion}</h2>
+        <h3 class="display-role" id="display-role">${card.role}</h3>
+        <h3 class="display-price" id="display-price">${card.price}</h3>
+         
+    </div>`)
+    console.log(card)})
+

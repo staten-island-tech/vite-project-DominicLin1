c3860cf12f84b115495ccd04c4e18aec002c9512
DominicLin1 <dominiclin777@gmail.com>
2022-12-22 09:52:07 -0500

    ....

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -3,38 +3,42 @@ import { League } from "./array.js";
 /*import { filter } from "./filter";*/
 console.log(League);
 const DOMselectors={
+    Space:document.getElementById("hi"),
 Menu:document.getElementById("List")}
 
 
 
-function insert(){
-let champion=League.champion
-let price=League.price
-let role=League.role
-let permaban=League.permaban
-let toxic=League.toxic}
-DOMselectors.Menu.addEventListener(
-    "click",
+
+    DOMselectors.Menu.addEventListener("click", function () {
+    //create()
+    League.forEach((work) => {
+ console.log(Space)
+        DOMselectors.Space.insertAdjacentHTML("afterend",
+        
+        `<div class="display-card">
+        <img class="display-img" src= ${work.img}/>
+        <h2 class="display-champion" id= "display-champion">${work.champion}</h2>
+        <h3 class="display-role" id="display-role">${work.role}</h3>
+        <h3 class="display-price" id="display-price">${work.price}</h3>
+        <h3 class="display-permaban" id="display-permaban">${work.permaban}</h3>
+        <h3 class="display-toxic" id="display-toxic">${work.toxic}</h3>
+        </div>`)
+         })
+})
+
 
 function create(){
+ League.forEach((work) => {
+ 
+DOMselectors.Space.insertAdjacentHTML("afterend",
 
-    const card = {
-        champion:League.forEach.champion((League.champion) ),
-        price:League.forEach.price((League.price)  ),
-        role:League.forEach.role((League.role) ),
-        permaban:League.forEach.permaban((League.permaban)  ),
-        toxic:League.forEach.toxic((League.toxic) ),
-        img:League.forEach.img((League.img) )}
-
-DOMselectors.Menu.insertAdjacentHTML(
-    "afterend",
-
-   `<div class="display-card">
-        <img class="display-img" src=${card.img}/>
-        <h2 class="display-champion" id= "display-champion">${card.champion}</h2>
-        <h3 class="display-role" id="display-role">${card.role}</h3>
-        <h3 class="display-price" id="display-price">${card.price}</h3>
-         
-    </div>`)
-    console.log(card)})
+`<div class="display-card">
+<img class="display-img" src= ${work.img}/>
+<h2 class="display-champion" id= "display-champion">${work.champion}</h2>
+<h3 class="display-role" id="display-role">${work.role}</h3>
+<h3 class="display-price" id="display-price">${work.price}</h3>
+<h3 class="display-permaban" id="display-permaban">${work.permaban}</h3>
+<h3 class="display-toxic" id="display-toxic">${work.toxic}</h3>
+</div>`)
+ })}
 

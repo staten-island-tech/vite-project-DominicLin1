32ff08dbd1629cc328fe94b4728c67669762466d
DominicLin1 <113190169+DominicLin1@users.noreply.github.com>
2023-01-02 22:27:06 -0500

    DONE

--- vite-project/js/array.js
+++ vite-project/js/array.js
@@ -4,85 +4,77 @@ export const League = [
     role: ["support"],
 
     price: 6300,
-    img: "https://img.redbull.com/images/c_crop,w_1280,h_640,x_0,y_0,f_auto,q_auto/c_scale,w_1200/redbullcom/2019/05/08/d24d8f2e-8972-466c-9e9f-979f332432fb/yuumi-lol-splash",
+    img: "https://images.contentstack.io/v3/assets/blt731acb42bb3d1659/blta312e7cca0e594d1/5db0601d2140ec675d68f4c1/RiotX_ChampionList_yuumi.jpg?quality=90&width=250",
   },
   {
     champion: "Irelia",
     role: ["top", "mid"],
 
     price: 4800,
-    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ04RU_DCUzDkRQ6fsv9PumO1_N4VW40sm_GPIIxUew0g&s",
+    img: "https://images.contentstack.io/v3/assets/blt731acb42bb3d1659/bltf5f2b8de93870aef/5db05fcea6470d6ab91ce59a/RiotX_ChampionList_irelia.jpg?quality=90&width=250",
   },
   {
     champion: "Jinx",
     role: ["adc"],
 
     price: 4800,
-    img: "https://i.kym-cdn.com/entries/icons/facebook/000/039/062/Jinx_0.jpg",
+    img: "https://images.contentstack.io/v3/assets/blt731acb42bb3d1659/blta2cba784d9fad4b8/5db05fce89fb926b491ed817/RiotX_ChampionList_jinx.jpg?quality=90&width=250",
   },
   {
     champion: "Teemo",
     role: ["top"],
 
     price: 1350,
-    img: "https://static.wikia.nocookie.net/leagueoflegends/images/5/5f/Teemo_OriginalCentered.jpg/revision/latest/scale-to-width-down/1280?cb=20180414203624",
+    img: "https://images.contentstack.io/v3/assets/blt731acb42bb3d1659/blt30ddbbdc0549078a/5db0600da6470d6ab91ce5b8/RiotX_ChampionList_teemo.jpg?quality=90&width=250",
   },
   {
     champion: "Akali",
     role: ["mid", "top"],
 
     price: 3150,
-    img: "https://cdn.oneesports.gg/cdn-data/wp-content/uploads/2019/07/TeamfightTactics_Akali.jpg",
+    img: "https://images.contentstack.io/v3/assets/blt731acb42bb3d1659/blt80ff58fe809777ff/5db05fa8adc8656c7d24e7d6/RiotX_ChampionList_akali.jpg?quality=90&width=250",
   },
   {
     champion: "Twisted Fate",
     role: ["mid"],
 
     price: 1350,
-    img: "https://static.wikia.nocookie.net/leagueoflegends/images/4/49/Twisted_Fate_OriginalCentered.jpg/revision/latest/scale-to-width-down/1280?cb=20180414203653",
-  },
-  {
-    champion: "Ryze",
-    role: ["mid"],
-
-    price: 450,
-    image:
-      "https://static.wikia.nocookie.net/leagueoflegends/images/e/ed/Ryze_OriginalCentered.jpg/revision/latest/scale-to-width-down/1280?cb=20180414203544",
+    img: "https://images.contentstack.io/v3/assets/blt731acb42bb3d1659/bltaebcdb8b21d440a7/5db0600ce9effa6ba5295fb3/RiotX_ChampionList_twistedfate.jpg?quality=90&width=250",
   },
   {
     champion: "Ezreal",
     role: ["adc"],
 
     price: 3150,
-    img: "https://static1-br.millenium.gg/entity_articles/2/55/02/@/65409-sa-ezreal-orig-1-orig-1.jpeg",
+    img: "https://images.contentstack.io/v3/assets/blt731acb42bb3d1659/blt60f687c95425f73f/5db05fbd2dc72966da746704/RiotX_ChampionList_ezreal.jpg?quality=90&width=250",
   },
   {
     champion: "Lee Sin",
     role: ["jungle"],
 
     price: 4800,
-    img: "https://static.wikia.nocookie.net/leagueoflegends/images/a/a8/Lee_Sin_OriginalCentered.jpg/revision/latest?cb=20180414203354",
+    img: "https://images.contentstack.io/v3/assets/blt731acb42bb3d1659/blt075d278529811445/5db05fde6af83b6d7032c8fe/RiotX_ChampionList_leesin.jpg?quality=90&width=250",
   },
   {
     champion: "Sejuani",
     role: ["jungle", "top"],
 
     price: 4800,
-    img: "https://cdn.vox-cdn.com/thumbor/nAgejTmGhnsFuV-CdVOjNcpzsrY=/0x0:1215x717/1400x1400/filters:focal(511x262:705x456):format(jpeg)/cdn.vox-cdn.com/uploads/chorus_image/image/54343885/Classic_Sejuani.0.jpg",
+    img: "https://images.contentstack.io/v3/assets/blt731acb42bb3d1659/blte4d521b893aa5a3e/5db05ffae9d7526ab429e544/RiotX_ChampionList_sejuani.jpg?quality=90&width=250",
   },
   {
     champion: "Lulu",
     role: ["support"],
 
     price: 4800,
-    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJPCvweVa0BFE3n1hAy3n27NS5vy6D5J0hShwAp-L83Q&s",
+    img: "https://images.contentstack.io/v3/assets/blt731acb42bb3d1659/blt424f04814efb3aca/5db05fdfe9d7526ab429e538/RiotX_ChampionList_lulu.jpg?quality=90&width=250",
   },
   {
     champion: "Draven",
     role: ["adc"],
 
     price: 4800,
-    img: "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Draven_0.jpg",
+    img: "https://images.contentstack.io/v3/assets/blt731acb42bb3d1659/bltc0be728e4cbb8f2a/5db05fbc89fb926b491ed80b/RiotX_ChampionList_draven.jpg?quality=90&width=250",
   },
 
   {
@@ -90,77 +82,77 @@ export const League = [
     role: ["support"],
 
     price: 4800,
-    img: "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Nautilus_0.jpg",
+    img: "https://images.contentstack.io/v3/assets/blt731acb42bb3d1659/bltf945a57aa16cea57/5db05fe7347d1c6baa57be37/RiotX_ChampionList_nautilus.jpg?quality=90&width=250",
   },
 
   {
     champion: "Vex",
     role: ["mid"],
-
+   
     price: 6300,
-    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxZWpibXZCDRA_rnq9LnsjXeobhihiIxTV3A2Zxo7--A&s",
+    img: "https://images.contentstack.io/v3/assets/blt731acb42bb3d1659/bltad313195d6b48daa/614e702764c8007a9bdec6b2/RiotX_ChampionList_vex.jpg?quality=90&width=250",
   },
   {
     champion: "Vi",
     role: ["jungle"],
-
+   
     price: 4800,
-    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQAIu32k_-QvxEQo-iF_7FteWc4eSO1Ai9a-3E0xgx&s",
+    img: "https://images.contentstack.io/v3/assets/blt731acb42bb3d1659/blt3bd79abf330fc807/5db06014dc674266df3d5d56/RiotX_ChampionList_vi.jpg?quality=90&width=250",
   },
   {
     champion: "Orianna",
     role: ["mid"],
 
     price: 4800,
-    img: "https://static.wikia.nocookie.net/leagueoflegends/images/8/8e/Orianna_OriginalCentered.jpg/revision/latest/scale-to-width-down/1280?cb=20180414203501",
+    img: "https://images.contentstack.io/v3/assets/blt731acb42bb3d1659/blt3318fc0e8103706d/5db05ff02140ec675d68f4a7/RiotX_ChampionList_orianna.jpg?quality=90&width=250",
   },
   {
     champion: "Wukong",
     role: ["top", "jungle"],
 
     price: 4800,
-    img: "https://static.wikia.nocookie.net/leagueoflegends/images/4/48/Wukong_OriginalSkin.jpg/revision/latest?cb=20181021060656",
+    img: "https://images.contentstack.io/v3/assets/blt731acb42bb3d1659/bltaf44673e1a16af30/5db05fe87d28b76cfe439807/RiotX_ChampionList_monkeyking.jpg?quality=90&width=250",
   },
   {
     champion: "Sion",
     role: ["top"],
 
     price: 1350,
-    img: "https://static.wikia.nocookie.net/leagueoflegends/images/f/fc/Sion_OriginalCentered.jpg/revision/latest/scale-to-width-down/1280?cb=20180414203549",
+    img: "https://images.contentstack.io/v3/assets/blt731acb42bb3d1659/blt7f28f1d689e4ad3d/5db06004adc8656c7d24e802/RiotX_ChampionList_sion.jpg?quality=90&width=250",
   },
   {
     champion: "Senna",
     role: ["support", "adc"],
 
     price: 6300,
-    img: "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Senna_0.jpg",
+    img: "https://images.contentstack.io/v3/assets/blt731acb42bb3d1659/blt24f4735ebde3c22b/5db08d642dc72966da74686e/RiotX_ChampionList_senna.jpg?quality=90&width=250",
   },
   {
     champion: "Rammus",
     role: ["jungle"],
 
     price: 1350,
-    img: "https://static.wikia.nocookie.net/leagueoflegends/images/3/3b/Rammus_OriginalCentered.jpg/revision/latest/scale-to-width-down/1280?cb=20180414203505",
+    img: "https://images.contentstack.io/v3/assets/blt731acb42bb3d1659/blt50cccb68a58349f5/5db05ffbdc674266df3d5d48/RiotX_ChampionList_rammus.jpg?quality=90&width=250",
   },
   {
     champion: "Vladimir",
     role: ["top"],
 
     price: 3150,
-    img: "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Vladimir_0.jpg",
+    img: "https://images.contentstack.io/v3/assets/blt731acb42bb3d1659/blt39ab5027f6fa1b81/5db0601589fb926b491ed82f/RiotX_ChampionList_vladimir.jpg?quality=90&width=250",
   },
   {
     champion: "Shaco",
     role: ["jungle"],
 
     price: 3150,
-    img: "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Shaco_0.jpg",
+    img: "https://images.contentstack.io/v3/assets/blt731acb42bb3d1659/bltc8b1d1ba926d01cc/5db060036e8b0c6d038c5cba/RiotX_ChampionList_shaco.jpg?quality=90&width=250",
   },
   {
     champion: "Pyke",
     role: ["support"],
 
     price: 6300,
-    img: "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Pyke_0.jpg",
+    img: "https://images.contentstack.io/v3/assets/blt731acb42bb3d1659/blt98269cb73e9fce70/5db05ff1347d1c6baa57be3d/RiotX_ChampionList_pyke.jpg?quality=90&width=250"
   },
 ];
